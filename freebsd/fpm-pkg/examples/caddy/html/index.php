86d93c875904a6fc840565f1003f57ec9b64e9a2
Dmitriy Kosolapov <freelance@dmkos.ru>
2025-08-11 09:45:36 +0300

    Add index.php

---
+++ freebsd/fpm-pkg/examples/caddy/html/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Service discovery</title>
+    </head>
+    <body>
+        <p><?= 'It works!' ?></p>
+    </body>
+</html>
